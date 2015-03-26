0594bff310a5c0534c692ccf40e9efa2aab2672d
Darren Labithiotis <dazzer13@gmail.com>
2015-03-26 23:14:13 +0000

    updated to use status code module

--- index.js
+++ index.js
@@ -19,36 +19,7 @@
  *
  */
 
-var codes = {
-	400: {
-		name: 'Bad Request',
-		message: 'The request could not be understood by the server due to malformed syntax.'
-	},
-	401: {
-		name: 'Unauthorized',
-		message: 'The request requires user authentication.'
-	},
-	403: {
-		name: 'Forbidden',
-		message: 'The server understood the request, but is refusing to fulfill it.'
-	},
-	404: {
-		name: 'Not Found',
-		message: 'The server has not found anything matching the Request-URI.'
-	},
-	405: {
-		name: 'Method Not Allowed',
-		message: 'The method specified in the Request-Line is not allowed for the resource.'
-	},
-	409: {
-		name: 'Conflict',
-		message: 'The request could not be completed due to a conflict with the current state of the resource.'
-	},
-	500: {
-		name: 'Internal Server Error',
-		message: 'The server encountered an unexpected condition which prevented it from fulfilling the request.'
-	}
-};
+var codes = require('status-codes');
 
 function JSONifyStack(stack){
 	if(typeof stack != 'string') return {};
